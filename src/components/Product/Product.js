ca6ee2efedf7884f0bce058f9edf0aa8250ae401
ashim8 <ashimshil54321@gmail.com>
2022-03-27 22:01:39 +0600

    fontawesome and click cart button added

---
+++ src/components/Product/Product.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import './Product.css';
+
+const Product = ({product, handleAddToCart}) => {
+    const {name,img,id, price} = product;
+    
+    return (
+        <div className='product'>
+            <img src={img} alt=""></img>
+            <div className='product-info'>
+                <p className='product-name'>{name}</p>
+                <p>ID:{id}</p>
+                <p>Price: ${price}</p>
+                
+            </div>
+            <button onClick={() => handleAddToCart(product)} className='btn-cart'>
+                <p className='btn-text'>Add to Cart</p>
+                <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
+            </button>
+        </div>
+    );
+};
+
+export default Product;
+<section>
+
+</section>
